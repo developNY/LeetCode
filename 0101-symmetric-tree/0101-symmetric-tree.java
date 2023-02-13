5dc57137cf7a72e3f6278d034d43518c7f8a1ef9
Nayeon Kim <sksldi0@gmail.com>
2023-02-13 00:34:13 -0600

    Time: 1 ms (27.16%), Space: 40.8 MB (35.52%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,17 +14,42 @@
  * }
  */
 class Solution {
-    public boolean isSame(TreeNode n1, TreeNode n2){
-        if(n1 == null && n2 == null)
-            return true;
-        else if(n1 == null || n2 == null)
-            return false;
-        else if(n1.val != n2.val)
-            return false;
-        return isSame(n1.left, n2.right) && isSame(n1.right, n2.left);
+    // Iterative - BFS
+    public boolean isSymmetric(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        
+        while(!queue.isEmpty()){
+            TreeNode ltmp = queue.poll();
+            TreeNode rtmp = queue.poll();
+            if(ltmp == null && rtmp == null)
+                continue;
+            else if(ltmp == null || rtmp == null)
+                return false;
+            else if(ltmp.val != rtmp.val)
+                return false;
+            queue.add(ltmp.left);
+            queue.add(rtmp.right);
+            queue.add(ltmp.right);
+            queue.add(rtmp.left);
+        }
+        return true;
     }
     
-    public boolean isSymmetric(TreeNode root) {
-        return isSame(root.left, root.right);
-    }
+    // Recursive - DFS
+//     public boolean isSame(TreeNode n1, TreeNode n2){
+//         if(n1 == null && n2 == null)
+//             return true;
+//         else if(n1 == null || n2 == null)
+//             return false;
+//         else if(n1.val != n2.val)
+//             return false;
+//         return isSame(n1.left, n2.right) && isSame(n1.right, n2.left);
+//     }
+    
+//     public boolean isSymmetric(TreeNode root) {
+//         return isSame(root.left, root.right);
+//     }
 }
