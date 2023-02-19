d50bde5b2b91d75aca9f067ada059d4367929178
Nayeon Kim <sksldi0@gmail.com>
2023-02-19 16:40:02 -0600

    Time: 0 ms (100.00%), Space: 42.3 MB (16.84%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,36 +14,15 @@
  * }
  */
 class Solution {
-
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        return dfs(root, null, null);
-    }
-
-    private boolean dfs(TreeNode root, Integer min, Integer max) {
-        if (root == null) return true;
-
-        if (
-            (min != null && root.val <= min) || max != null && root.val >= max
-        ) {
+    public boolean isValid(TreeNode root, Integer min, Integer max){
+        if(root == null)
+            return true;
+        if((min != null && root.val <= min) || (max != null && root.val >= max))
             return false;
-        }
-
-        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
     }
 }
-// class Solution {
-//     public boolean isValid(TreeNode root, int min, int max){
-//         if(root == null)
-//             return true;
-//         if(root.val <= min || root.val >= max)
-//             return false;
-//         return isValid(root.left, min, Math.min(max, root.val)) && isValid(root.right, Math.max(min, root.val), max);
-//     }
-    
-//     public boolean isValidBST(TreeNode root) {
-//         if(root.left == null && root.right == null)
-//                 return true;
-//         return isValid(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-//     }
-// }
