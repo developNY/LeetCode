0a77ccf45222d9868339eea18be8184b069cfa9b
Nayeon Kim <sksldi0@gmail.com>
2023-02-22 22:20:57 -0600

    Time: 2 ms (86.52%), Space: 43.3 MB (5.06%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,26 +1,16 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
         int res = text.length();
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('b', 0);
-        map.put('a', 0);
-        map.put('l', 0);
-        map.put('o', 0);
-        map.put('n', 0);
-        
+        int[] alpha = new int[26];
         for(int i=0; i<text.length(); i++){
             char c = text.charAt(i);
-            if(map.containsKey(c))
-                map.put(c, map.get(c)+1);
-            else
-                map.put(c, 1);
+            alpha[c-'a']++;
         }
-        for(Map.Entry<Character, Integer> e : map.entrySet()){
-            if(e.getKey() == 'b' || e.getKey() == 'a' || e.getKey() == 'n')
-                res = Math.min(res, map.get(e.getKey()));
-            else if(e.getKey() == 'l' || e.getKey() == 'o')
-                res = Math.min(res, map.get(e.getKey())/2);
-        }
+        res = Math.min(res, alpha['b'-'a']);
+        res = Math.min(res, alpha['a'-'a']);
+        res = Math.min(res, alpha['l'-'a']/2);
+        res = Math.min(res, alpha['o'-'a']/2);
+        res = Math.min(res, alpha['n'-'a']);
         return res;
     }
 }
