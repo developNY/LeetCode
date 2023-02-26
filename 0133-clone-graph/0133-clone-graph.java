6be9edd0fc30560651c375ddf89161a7da3981b4
Nayeon Kim <sksldi0@gmail.com>
2023-02-26 03:11:00 -0600

    Time: 27 ms (24.18%), Space: 42.3 MB (78.24%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,17 +19,17 @@ class Node {
 */
 
 class Solution {
-
-    public HashMap<Integer, Node> map = new HashMap<>();
-
+    Map<Integer, Node> nodes = new HashMap<>();
+    
     public Node cloneGraph(Node node) {
-        if (node == null) return null;
-        if (map.containsKey(node.val)) return map.get(node.val);
+        if(node == null)
+            return null;
+        if(nodes.containsKey(node.val))
+            return nodes.get(node.val);
         Node newNode = new Node(node.val, new ArrayList<Node>());
-        map.put(node.val, newNode);
-        for (Node neighbor : node.neighbors) {
-            newNode.neighbors.add(cloneGraph(neighbor));
-        }
+        nodes.put(node.val, newNode);
+        for(Node n : node.neighbors)
+            newNode.neighbors.add(cloneGraph(n));
         return newNode;
     }
 }
