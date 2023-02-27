64b90b18922c3510f3f7bcf3197d2e9ddf9a762c
Nayeon Kim <sksldi0@gmail.com>
2023-02-26 23:53:26 -0600

    Time: 2 ms (84.34%), Space: 42.1 MB (30.29%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,83 +1,64 @@
-class Solution {
+class Solution {  
+    int[][] grid = null;
+    boolean[][] visited = null;
+    Queue<List<Integer>> queue = new LinkedList<>();
+    
+    public void rotten(int row, int col){
+        if(row<0 || row>= grid.length || col<0 || col>= grid[0].length || visited[row][col] || grid[row][col] == 0)
+            return;
+        
+        if(grid[row][col] == 1 && !visited[row][col]){
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(row);
+            tmp.add(col);
+            queue.add(tmp);
+            grid[row][col] = 2;
+            visited[row][col] = true;
+        }
+    }
+    
     public int orangesRotting(int[][] grid) {
-        // This can be solved using BFS. Basically, to find out the max depth to which BFS can go, where at each depth, the adjacent oranges can be rotten. Note that if there are multiple components, we need to find if there are any rotten oranges. Hence to begin with, we will consider all existing rotten oranges. 
-        Queue<int[]> q = new LinkedList<>();
-        int total = 0;
-        // Initially, traverse the whole grid and find out which ones are rotten
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                int current = grid[i][j];
-                if (current == 1 || current  == 2) total ++;
-                if (current == 2) {
-                    int[]a = new int[2];
-                    a[0] = i;
-                    a[1] = j;
-                    q.add(a);
+        int res = 0;
+        int row = grid.length, col = grid[0].length;
+        this.grid = grid;
+        this.visited = new boolean[row][col];
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == 2){
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(i);
+                    tmp.add(j);
+                    queue.add(tmp);
+                    visited[i][j] = true;
                 }
             }
         }
-
-        int current = 0;
-        // Now perform BFS
-        int depth = 0;
-        while (!q.isEmpty()) {
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
             
-            int len = q.size();
-            current += len;
-            if (current == total) return depth;
-            depth++;
-            for (int i = 0; i < len; i++) {
-                int[]a = q.remove();
-
-                int x = a[0];
-                int y = a[1];
-                // Top
-                if (x > 0 && grid[x-1][y] == 1) {
-                    grid[x-1][y] = 2;
-                    int[]b = new int[2];
-                    b[0] = x-1;
-                    b[1] = y;
-                    q.add(b);
-                }
-
-                // Left
-                if (y > 0 && grid[x][y-1] == 1) {
-                    grid[x][y-1] = 2;
-                    int[]b = new int[2];
-                    b[0] = x;
-                    b[1] = y-1;
-                    q.add(b);
-                }
-
-                // Bottom
-                if (x < grid.length - 1 && grid[x+1][y] == 1) {
-                    grid[x+1][y] = 2;
-                    int[]b = new int[2];
-                    b[0] = x+1;
-                    b[1] = y;
-                    q.add(b);
-                }
-
-                // Right
-                if (y < grid[0].length - 1 && grid[x][y+1] == 1) {
-                    grid[x][y+1] = 2;
-                    int[]b = new int[2];
-                    b[0] = x;
-                    b[1] = y+1;
-                    q.add(b);
-                }
+            for(int i=0; i<size; i++){
+                List<Integer> tmp = queue.poll();
+                int tmpRow = tmp.get(0);
+                int tmpCol = tmp.get(1);
+                
+                rotten(tmpRow+1, tmpCol);
+                rotten(tmpRow-1, tmpCol);
+                rotten(tmpRow, tmpCol+1);
+                rotten(tmpRow, tmpCol-1);
             }
+            res++;
         }
-
-        // Now find out if there is any organe left that's not rotten
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == 1){
                     return -1;
                 }
             }
         }
-
-        return depth;
+        
+        return res == 0? 0 : res-1;
     }
 }
