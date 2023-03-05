49da20e600da67de77c1f91f0467e42da6b0c385
Nayeon Kim <sksldi0@gmail.com>
2023-03-05 03:25:27 -0600

    Time: 193 ms (67.23%), Space: 40.7 MB (47.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,30 +2,33 @@ class Solution {
 	public boolean exist(char[][] board, String word) {
 		for(int r = 0; r < board.length; r++){
 			for(int c = 0; c < board[0].length; c++){
-				if(dfsBoard(board, new boolean[board.length][board[0].length], r, c, word, 0)){                                               return true;
-				}
+				if(dfs(r, c, 0, board, word, new boolean[board.length][board[0].length]))                                                   return true;
 			}
 		}
 		return false;    
 	}
 
-	public boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {        
-		if(wordInd >= word.length())
+	public boolean dfs(int row, int col, int wordidx, char[][] board, String word, boolean[][] visited) {        
+		if(wordidx >= word.length())
             return true;
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || visitedBoard[row][col] || board[row][col] != word.charAt(wordInd) )
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(wordidx) )
 			return false;         
             
 		//if(wordInd == word.length() - 1){ return true; }
 		
-        visitedBoard[row][col] = true;                                                                // mark it as visited now     
+        visited[row][col] = true;                                                                // mark it as visited now     
         
-		if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1) ){ return true; }            // DFS on Below
-        if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1) ){ return true; }            // DFS on ABove	
-		if( dfsBoard(board, visitedBoard, row, col-1, word, wordInd + 1) ){ return true; }            // DFS on Left
-		if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1) ){ return true; }            // DFS on Right
+		if(dfs(row+1, col, wordidx+1, board, word, visited))
+            return true;
+        if(dfs(row-1, col, wordidx+1, board, word, visited))
+            return true;
+        if(dfs(row, col+1, wordidx+1, board, word, visited))
+            return true;
+        if(dfs(row, col-1, wordidx+1, board, word, visited))
+            return true;
 		
 		
-		visitedBoard[row][col] = false;
+		visited[row][col] = false;
 		return false;
 	}
 }
