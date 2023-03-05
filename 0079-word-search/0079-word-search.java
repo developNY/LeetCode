1f7dd251804abcda5c1ff885161ca674c81424e7
Nayeon Kim <sksldi0@gmail.com>
2023-03-05 03:13:41 -0600

    Time: 197 ms (63.19%), Space: 40.2 MB (89.19%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,29 @@
+class Solution {
+	public boolean exist(char[][] board, String word) {
+		for(int r = 0; r < board.length; r++){
+			for(int c = 0; c < board[0].length; c++){
+				if(board[r][c] == word.charAt(0)){                                                // keep iterating until we don't find first character of "word"
+					boolean isFound = dfsBoard(board, new boolean[board.length][board[0].length], r, c, word, 0);    // call DFS or recursion
+					if(isFound){ return true; }
+				}
+			}
+		}
+		return false;                                                                             // if we traveresed all chars of board, it means we did not find all chars of word. so return false
+	}
+	// ----------------------------------------------------------------------------------------------------------------------------------------------- //
+	public boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {        			
+		if( (row < 0 || row >= board.length)   ||   (col < 0 || col >= board[0].length)  ||   board[row][col] != word.charAt(wordInd) ){
+			return false;                                                                             // return false, if:  a)  row or col goes out of bound.  b) char in board != char in word 
+		}
+		if(visitedBoard[row][col] ){ return false; }                                                  // this element already visited, so don't do DFS on this
+		if(wordInd == word.length() - 1){ return true; }
+		visitedBoard[row][col] = true;                                                                // mark it as visited now             
+			
+		if( dfsBoard(board, visitedBoard, row, col-1, word, wordInd + 1) ){ return true; }            // DFS on Left
+		if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1) ){ return true; }            // DFS on Right
+		if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1) ){ return true; }            // DFS on ABove
+		if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1) ){ return true; }            // DFS on Below
+		visitedBoard[row][col] = false;
+		return false;
+	}
+}
