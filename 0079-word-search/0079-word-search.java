a3027b88a858d24ad7811c02214830b45bf4cba9
Nayeon Kim <sksldi0@gmail.com>
2023-03-05 03:27:57 -0600

    Time: 194 ms (65.96%), Space: 40.5 MB (55.96%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,13 +1,15 @@
 class Solution {
-	public boolean dfs(int row, int col, int wordidx, char[][] board, String word, boolean[][] visited) {       
-		if(wordidx >= word.length())
+    public boolean dfs(int row, int col, int wordidx, char[][] board, String word, boolean[][] visited){
+        if(wordidx >= word.length())
             return true;
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(wordidx) )
-			return false;         
 
+        if(row<0 || row>=board.length || col<0 || col>=board[0].length || visited[row][col] || (board[row][col] != word.charAt(wordidx)))
+            return false;
+        
         visited[row][col] = true;
         
-		if(dfs(row+1, col, wordidx+1, board, word, visited))
+
+        if(dfs(row+1, col, wordidx+1, board, word, visited))
             return true;
         if(dfs(row-1, col, wordidx+1, board, word, visited))
             return true;
@@ -15,17 +17,17 @@ class Solution {
             return true;
         if(dfs(row, col-1, wordidx+1, board, word, visited))
             return true;
-		
-		visited[row][col] = false;
-		return false;
-	}
-    
+        visited[row][col] = false;
+        return false;
+    }
+
     public boolean exist(char[][] board, String word) {
-		for(int i = 0; i < board.length; i++){
-			for(int j = 0; j < board[0].length; j++){
-				if(dfs(i, j, 0, board, word, new boolean[board.length][board[0].length]))                                                   return true;
-			}
-		}
-		return false;    
-	}
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(dfs(i, j, 0, board, word, new boolean[board.length][board[0].length]))
+                    return true;
+            }
+        }
+        return false;
+    }
 }
