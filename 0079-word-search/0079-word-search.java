ec543e844ecddb040322f5ef3b4cfdeb0ab3bdb1
Nayeon Kim <sksldi0@gmail.com>
2023-03-05 03:26:14 -0600

    Time: 195 ms (65.06%), Space: 40.9 MB (38.34%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -24,9 +24,9 @@ class Solution {
 	}
     
     public boolean exist(char[][] board, String word) {
-		for(int r = 0; r < board.length; r++){
-			for(int c = 0; c < board[0].length; c++){
-				if(dfs(r, c, 0, board, word, new boolean[board.length][board[0].length]))                                                   return true;
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(dfs(i, j, 0, board, word, new boolean[board.length][board[0].length]))                                                   return true;
 			}
 		}
 		return false;    
