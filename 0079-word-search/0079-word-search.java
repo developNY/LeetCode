8efd7fc0efbd5a502112b20f2fd30d90898f0868
Nayeon Kim <sksldi0@gmail.com>
2023-03-05 03:13:52 -0600

    Time: 199 ms (61.01%), Space: 40.1 MB (94.40%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,9 +1,14 @@
 class Solution {
 	public boolean exist(char[][] board, String word) {
-		for(int r = 0; r < board.length; r++){
-			for(int c = 0; c < board[0].length; c++){
+		int rows = board.length, cols = board[0].length;                                // length of rows & columns 
+		if(word.length() == 0){ return true; }                                          // if "word" is empty, we will just return true. 
+		if(rows * cols < word.length()){ return false; }                                // if board size (rows*cols) < word length, => word has some character(s) not present in board. So, just return False. This is optimized way as we are avoiding doing DFS search in such test cases. 
+
+		// Traverse all chars of board
+		for(int r = 0; r < rows; r++){
+			for(int c = 0; c < cols; c++){
 				if(board[r][c] == word.charAt(0)){                                                // keep iterating until we don't find first character of "word"
-					boolean isFound = dfsBoard(board, new boolean[board.length][board[0].length], r, c, word, 0);    // call DFS or recursion
+					boolean isFound = dfsBoard(board, new boolean[rows][cols], r, c, word, 0);    // call DFS or recursion
 					if(isFound){ return true; }
 				}
 			}
