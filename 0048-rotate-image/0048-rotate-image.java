2c5605ed36e2a2db2d2b30522316db6fd795c4b6
Nayeon Kim <sksldi0@gmail.com>
2023-03-10 02:10:41 -0600

    Time: 0 ms (100.00%), Space: 43.4 MB (5.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,33 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int l = 0;
-        int r = matrix.length - 1;
+        int l = 0, r = matrix[0].length-1;
         
-        while ( l < r )
-        {
-            for(int i = 0; i < r - l; i++)
-            {
-                int top = l;
-                int bottom = r;
-                 //save the topleft
-                int topLeft = matrix[top][l + i];
-
-                //move bottom left into top left
-                matrix[top][l + i] = matrix[bottom - i][l];
-
-                // move bottom right into bottom left
-                matrix[bottom - i][l] = matrix[bottom][r - i];
-
-                // move top right into bottom right
-                matrix[bottom][r - i] = matrix[top + i][r];
-
-                // move top left into top right
-                matrix[top + i][r] = topLeft;
+        while(l<r){
+            for(int i=0; i<r-l; i++){
+                int top = l, bottom = r;
                 
+                int tmp = matrix[top][l+i];
+                
+                matrix[top][l+i] = matrix[bottom-i][l];
+                
+                matrix[bottom-i][l] = matrix[bottom][r-i];
+                
+                matrix[bottom][r-i] = matrix[top+i][r];
+                
+                matrix[top+i][r] = tmp;
             }
             
-            r -= 1;
-            l += 1;
+            l++;
+            r--;
         }
     }
 }
