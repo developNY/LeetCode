a4e4f18f69887635fb1f870d089dbc96f76e1d3e
Nayeon Kim <sksldi0@gmail.com>
2023-04-01 03:31:06 -0500

    Time: 3 ms (68.17%), Space: 42.5 MB (73.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,42 +3,39 @@ class Solution {
         Set<Character> set = new HashSet<>();
         for(int i=row; i<row+3; i++){
             for(int j=col; j<col+3; j++){
-                if(board[i][j] != '.'){
-                    if(set.contains(board[i][j]))
-                        return false;
+                if(board[i][j] != '.' && set.contains(board[i][j]))
+                    return false;
+                if(board[i][j] != '.')
                     set.add(board[i][j]);
-                }
             }
         }
         return true;
     }
     
-    public boolean isValidSudoku(char[][] board) {
-        // row check
+    public boolean isValidSudoku(char[][] board){
+        //row check
         for(int i=0; i<board.length; i++){
             Set<Character> set = new HashSet<>();
             for(int j=0; j<board[0].length; j++){
-                if(board[i][j] != '.'){
-                    if(set.contains(board[i][j]))
-                        return false;
+                if(board[i][j] != '.' && set.contains(board[i][j]))
+                    return false;
+                if(board[i][j] != '.')
                     set.add(board[i][j]);
-                }
             }
         }
         
-        // col check
+        //col check
         for(int i=0; i<board.length; i++){
             Set<Character> set = new HashSet<>();
             for(int j=0; j<board[0].length; j++){
-                if(board[j][i] != '.'){
-                    if(set.contains(board[j][i]))
-                        return false;
+                if(board[j][i] != '.' && set.contains(board[j][i]))
+                    return false;
+                if(board[j][i] != '.')
                     set.add(board[j][i]);
-                }
             }
         }
         
-        // 3*3 box check
+        //3*3 check
         for(int i=0; i<board.length; i=i+3){
             for(int j=0; j<board[0].length; j=j+3){
                 if(!checkBox(i, j, board))
