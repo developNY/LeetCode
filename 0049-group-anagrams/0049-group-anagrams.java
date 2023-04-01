a04e2cdc578c73038ef952713ad9680111700b7b
Nayeon Kim <sksldi0@gmail.com>
2023-03-31 23:00:08 -0500

    Time: 25 ms (15.80%), Space: 46.6 MB (25.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for(String str : strs){
-            int[] tmp = new int[26];
-            for(char c : str.toCharArray()){
-                tmp[c-'a']++;
-            }
-        
+            int[] arr = new int[26];
             StringBuilder sb = new StringBuilder();
-            for(int i : tmp){
-                sb.append('#');
+            
+            for(char c : str.toCharArray())
+                arr[c-'a']++;
+            
+            for(int i : arr){
+                sb.append("#");
                 sb.append(i);
             }
+            
             if(!map.containsKey(sb.toString()))
                 map.put(sb.toString(), new ArrayList());
             map.get(sb.toString()).add(str);
         }
+        
         return new ArrayList(map.values());
     }
 }
