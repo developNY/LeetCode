21c7a2e47c5e4b4697bc96a74a5a3bf30dfbf4b5
Nayeon Kim <sksldi0@gmail.com>
2023-05-14 22:58:31 -0500

    Time: 2 ms (89.21%), Space: 40.7 MB (54.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        char c = s.charAt(0);
+        if(s.length()%2 != 0 || c == ')' || c == ']' || c == '}')
+            return false;
+        
+        Stack<Character> stack = new Stack<>();
+        stack.add(c);
+        
+        for(int i=1; i<s.length(); i++){
+            char c2 = s.charAt(i);
+            if(c2==')' || c2=='}' || c2==']'){
+                if(stack.isEmpty())
+                    return false;
+                char pop = stack.pop();
+                if(c2==')' && pop != '(')
+                    return false;
+                else if(c2==']' && pop != '[')
+                    return false;
+                else if(c2=='}' && pop != '{')
+                    return false;
+            }else
+                stack.add(c2);
+        }
+        
+        if(stack.isEmpty())
+            return true;
+        return false;
+    }
+}
