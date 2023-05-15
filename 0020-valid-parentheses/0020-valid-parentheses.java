c8f692dbbaff59836e2783f102be44d13765409a
Nayeon Kim <sksldi0@gmail.com>
2023-05-14 23:04:30 -0500

    Time: 2 ms (89.21%), Space: 40.5 MB (77.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
+        char c = s.charAt(0);
+        if(s.length()%2 != 0 || c == ')' || c == ']' || c == '}')
+            return false;
+        
         Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '['){
-                stack.push(c);
-            }else{
-                if(stack.size() == 0)
+            char c2 = s.charAt(i);
+            if(c2==')' || c2=='}' || c2==']'){
+                if(stack.isEmpty())
                     return false;
-                char p = stack.pop();
-                if(c == ')' && p != '(')
+                char pop = stack.pop();
+                if((c2==')' && pop != '(') || (c2==']' && pop != '[') ||(c2=='}' && pop != '{'))
                     return false;
-                else if(c == '}' && p != '{')
-                    return false;
-                else if(c == ']' && p != '[')
-                    return false;
-            }
+            }else
+                stack.add(c2);
         }
         
-        return stack.isEmpty();
+        if(stack.isEmpty())
+            return true;
+        return false;
     }
 }
