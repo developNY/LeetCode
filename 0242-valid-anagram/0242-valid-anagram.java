2e2eefaf1ae2cd50ed2ea39c3d08e984a3771c28
Nayeon Kim <sksldi0@gmail.com>
2023-06-02 01:19:41 -0500

    Time: 2 ms (99.76%), Space: 43.5 MB (22.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,16 +4,15 @@ class Solution {
             return false;
         
         int[] arr = new int[26];
-
-        for(int i=0; i<s.length(); i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
-        }
         
-        for(int i : arr)
+        for(char c : s.toCharArray())
+            arr[c-'a']++;
+        for(char c : t.toCharArray())
+            arr[c-'a']--;
+        for(int i : arr){
             if(i != 0)
                 return false;
-        
+        }
         return true;
     }
-}
+} 
