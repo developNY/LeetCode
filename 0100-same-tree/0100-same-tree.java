7f72929af5e8fa4491f3d536968e305a0c4627cf
Nayeon Kim <sksldi0@gmail.com>
2023-06-13 16:25:59 -0500

    Time: 0 ms (100%), Space: 40.2 MB (56.29%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,57 +14,13 @@
  * }
  */
 class Solution {
-    // Iteration - BFS
-    public boolean check(TreeNode p, TreeNode q) {
-    if (p == null && q == null) 
-        return true;
-    if (q == null || p == null) 
-        return false;
-    if (p.val != q.val) 
-        return false;
-    return true;
-  }
-
-  public boolean isSameTree(TreeNode p, TreeNode q) {
-    if (p == null && q == null) 
-        return true;
-    if (!check(p, q)) 
-        return false;
-
-    ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
-    ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
-    deqP.addLast(p);
-    deqQ.addLast(q);
-
-    while (!deqP.isEmpty()) {
-      p = deqP.removeFirst();
-      q = deqQ.removeFirst();
-
-      if (!check(p, q)) return false;
-      if (p != null) {
-        if (!check(p.left, q.left)) return false;
-        if (p.left != null) {
-          deqP.addLast(p.left);
-          deqQ.addLast(q.left);
-        }
-        if (!check(p.right, q.right)) return false;
-        if (p.right != null) {
-          deqP.addLast(p.right);
-          deqQ.addLast(q.right);
-        }
-      }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+        else if (p == null || q == null)
+            return false;
+        else if(p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-    return true;
-  }
-    
-    // Recursive - DFS
-    // public boolean isSameTree(TreeNode p, TreeNode q) {
-    //     if(p == null && q == null)
-    //         return true;
-    //     else if(p == null || q == null)
-    //         return false;
-    //     if(p.val != q.val)
-    //         return false;
-    //     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    // }
 }
